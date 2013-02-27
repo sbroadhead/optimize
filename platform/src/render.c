33a1980cc619cca3b70142989f23168dca3bce62
scbgh <sbroadhead@gmail.com>
2013-02-26 20:22:57 -0500

    Add rendering functions

--- platform/src/render.c
+++ platform/src/render.c
@@ -3,9 +3,41 @@
  * Implementations of rendering functions.
  */
 
-#include <SDL.h>
-#include <SDL_ttf.h>
+#include <math.h>
 #include "render.h"
 
+/*
+ * Draw an arrowhead representing a vector
+ */
+void draw_vector(double x, double y, double length, double angle) 
+{
+    glPushMatrix();
+    glTranslated(x, y, 0.0);
+    glRotated(angle, 0.0, 0.0, 1.0);
+    glScaled(length, length, 0.0);
 
+    glBegin(GL_LINE_LOOP);
+        glVertex2d(-0.5, 0.25);
+        glVertex2d(0.5, 0.0);
+        glVertex2d(-0.5, -0.25);
+    glEnd();
 
+    glPopMatrix();
+}
+
+/*
+ * Draw a polygon
+ */
+void draw_polygon(double *pts, int count)
+{
+    glPushMatrix();
+
+    int i;
+    glBegin(GL_LINE_LOOP);
+        for (i = 0; i < count; i++) {
+            glVertex2d(pts[2*i], pts[2*i+1]);
+        }
+    glEnd();
+
+    glPopMatrix();
+}
--- platform/src/render.h
+++ platform/src/render.h
@@ -6,7 +6,16 @@
 #ifndef _RENDER_H_
 #define _RENDER_H_
 
+#if defined __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#else
+#include <gl/gl.h>
+#include <gl/glu.h>
+#endif
 
+void draw_vector(double x, double y, double length, double angle);
+void draw_polygon(double *pts, int count);
 
 #endif
 
