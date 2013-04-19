f8ebc58033d9a73934df62c471e061784998fd02
scbgh <sbroadhead@gmail.com>
2013-04-19 00:54:20 -0400

    Test of DSL is now 6-dimensional Rosenbrock banana function

--- experiments/lbfgs/test_lbfgs.c
+++ experiments/lbfgs/test_lbfgs.c
@@ -14,8 +14,14 @@ static int progress(void *instance, const lbfgsfloatval_t *x, const lbfgsfloatva
         const lbfgsfloatval_t xnorm, const lbfgsfloatval_t gnorm, const lbfgsfloatval_t step,
         int n, int k, int ls)
 {
+    int i;
+
     printf("Iteration %d:\n", k);
-    printf("  fx = %f, a = %f, b = %f, c = %f\n", fx, x[0], x[1], x[2]);    
+    printf("  fx = %f; ", fx);
+    for (i = 0; i < n; i ++){
+        printf("x[%d] = %f; ", i, x[i]);
+    }
+    printf("\n");
     printf("  xnorm = %f, gnorm = %f, step = %f\n", xnorm, gnorm, step);
     printf("\n");
     return 0;
@@ -24,8 +30,9 @@ static int progress(void *instance, const lbfgsfloatval_t *x, const lbfgsfloatva
 int main(int argc, char *argv[])
 {
     int i, ret = 0;
+    int n = 6;
     lbfgsfloatval_t fx;
-    lbfgsfloatval_t *x = lbfgs_malloc(3);
+    lbfgsfloatval_t *x = lbfgs_malloc(n);
     lbfgs_parameter_t param;
 
     if (!x) {
@@ -33,18 +40,22 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    for (i = 0; i < 3; i++) {
-        x[i] = 1;
+    for (i = 0; i < n; i++) {
+        x[i] = 4;
     }
 
     /* Initialize the parameters for the L-BFGS optimization. */
     lbfgs_parameter_init(&param);
     //param.linesearch = LBFGS_LINESEARCH_BACKTRACKING_STRONG_WOLFE;
 
-    ret = lbfgs(3, x, &fx, evaluate, progress, NULL, &param);
+    ret = lbfgs(n, x, &fx, evaluate, progress, NULL, &param);
 
     printf("L-BFGS optimization terminated with status code = %d\n", ret);
-    printf("  fx = %f, a = %f, b = %f, c = %f\n", fx, x[0], x[1], x[2]);    
+    printf("  fx = %f; ", fx);
+    for (i = 0; i < n; i ++){
+        printf("x[%d] = %f; ", i, x[i]);
+    }
+    printf("\n");
 
     /* Answer according to Wolfram Alpha:
      *  1.00607 x^2 - 0.363643 x + 0.554
