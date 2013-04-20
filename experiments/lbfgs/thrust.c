eada931de0db9800df48bfb6de4c00074cea3d64
scbgh <sbroadhead@gmail.com>
2013-04-20 13:18:19 -0400

    Preliminary L-BFGS solver for iceberg is working, and MATLAB code will visualize the output

--- experiments/lbfgs/thrust.c
+++ experiments/lbfgs/thrust.c
@@ -20,7 +20,7 @@ typedef struct {
 } vals_t;
 
 vals_t vals;
-double mu = 1e-3;
+double mu = 1e-5;
 
 static double grad(double (*func)(const double *), const double *x, int var)
 {
@@ -39,81 +39,107 @@ static double grad(double (*func)(const double *), const double *x, int var)
     return 0.5 * hinv * (f1 - f2);
 }
 
-static double lb(double x, double y)
+static double lb(double x, double y) /* log barrier */
 {
     return -mu * (y <= x) ? HUGE_VAL : log(y - x);
 }
 
-static double qp(double x, double y)
+static double qp(double x, double y) /* quadratic penalty */
 {
     return mu * (y <= x) ? (y-x)*(y-x) : 0;
 }
 
-static double ib(double x, double y)
+static double ib(double x, double y) /* inverse barrier */
 {
     return mu * (y <= x) ? HUGE_VAL : 1/(y - x);
 }
 
+static double sqdist(double x, double y) { return (x-y)*(x-y); }
+static double dist(double x, double y) { return fabs(x-y); }
+static double sqdist2(double x1, double y1, double x2, double y2) { return sqdist(x1, x2)+sqdist(y1, y2); }
+static double dist2(double x1, double y1, double x2, double y2) { return sqrt(sqdist2(x1, y1, x2, y2)); }
+
 static double f(const double *x)
 {
-    double px[N], py[N], vx[N], vy[N], dx[N], dy[N], w[N];
+    const int iters = 10;
+    double h = DT / (double)iters;
+
+    double px[2], py[2], vx[2], vy[2], dx[2], dy[2], w[2];
     double ax, ay;
     double dw;
     double theta;
-    double dist;
     double dnorm;
     double y = 0;
     double cons = 0.0;
+    double pdist;
+    double angle;
     int ccount = 0;
-    int i;
+    int i, j;
+
+    int cur = 1;
+    int prev = 0;
     
-    px[0] = vals.px0; py[0] = vals.py0;
-    vx[0] = vals.vx0; vy[0] = vals.vy0;
-    dx[0] = vals.dx0; dy[0] = vals.dy0;
-    w[0] = vals.w0;
+    px[prev] = vals.px0; py[prev] = vals.py0;
+    vx[prev] = vals.vx0; vy[prev] = vals.vy0;
+    dx[prev] = vals.dx0; dy[prev] = vals.dy0;
+    w[prev] = vals.w0;
+
+    cur = 1;
+    prev = 0;
 
 #define CONSTRAINT(x, y) do{double c=ib(x,y)/(ccount+1);\
     double cc=(ccount?(cons/(1.0+1.0/ccount)):0);\
     cons=c+cc;ccount++;}while(0)
 
-    for (i = 1; i < N; i++) {
-        /* angular velocity */
-        dw = 2*(x[L+i-1] - x[R+i-1])/(vals.radius*vals.mass);
-        w[i] = w[i-1] + dw * DT;
-        /* direction */
-        theta = 0.5 * DT * (w[i] + w[i-1]);
-        dx[i] = dx[i-1] * cos(theta) - dy[i-1] * sin(theta);
-        dy[i] = dx[i-1] * sin(theta) + dy[i-1] * cos(theta);
-        dnorm = sqrt(dx[i]*dx[i] + dy[i]*dy[i]);
-        dx[i] /= dnorm;
-        dy[i] /= dnorm;
+    for (i = 1; i <= N; i++) {
+        for (j = 0; j < iters; j++) {
+            /* angular velocity */
+            dw = 2*(x[L+i-1] - x[R+i-1])/(vals.radius*vals.mass);
+            w[cur] = w[prev] + dw * h;
+            /* direction */
+            theta = 0.5 * h * (w[cur] + w[prev]);
+            dx[cur] = dx[prev] * cos(theta) - dy[prev] * sin(theta);
+            dy[cur] = dx[prev] * sin(theta) + dy[prev] * cos(theta);
+            dnorm = sqrt(dx[cur]*dx[cur] + dy[cur]*dy[cur]);
+            dx[cur] /= dnorm;
+            dy[cur] /= dnorm;
 
-        /* acceleration*/
-        ax = dx[i-1] * (x[L+i-1] + x[R+i-1])/vals.mass;
-        ay = dy[i-1] * (x[L+i-1] + x[R+i-1])/vals.mass;
-        /* velocity */
-        vx[i] = vx[i-1] + ax * DT;
-        vy[i] = vy[i-1] + ay * DT;
-        /* position */
-        px[i] = px[i-1] + vx[i-1] * DT + 0.5 * ax * DT * DT;
-        py[i] = py[i-1] + vy[i-1] * DT + 0.5 * ay * DT * DT;
-        
-        /* distance from destination */
-        dist = ((px[i] - vals.pxn) * (px[i] - vals.pxn) + (py[i] - vals.pyn) * (py[i] - vals.pyn))/N;
-        y += dist;
-
-        dist = x[L+i] - x[L+i-1];
-        CONSTRAINT(dist*dist, DT*DT);
-        dist = x[R+i] - x[R+i-1];
-        CONSTRAINT(dist*dist, DT*DT);
+            /* acceleration*/
+            ax = dx[prev] * (x[L+i-1] + x[R+i-1])/vals.mass;
+            ay = dy[prev] * (x[L+i-1] + x[R+i-1])/vals.mass;
+            /* velocity */
+            vx[cur] = vx[prev] + ax * h;
+            vy[cur] = vy[prev] + ay * h;
+            /* position */
+            px[cur] = px[prev] + vx[prev] * h + 0.5 * ax * h * h;
+            py[cur] = py[prev] + vy[prev] * h + 0.5 * ay * h * h;
+            
+            /* distance from destination */
+            y += h * ((px[cur] - vals.pxn) * (px[cur] - vals.pxn) + (py[cur] - vals.pyn) * (py[cur] - vals.pyn));
+    
+            pdist = sqdist2(px[prev], py[prev], vals.pxn, vals.pyn);
+            CONSTRAINT(sqdist2(px[cur], py[cur], vals.pxn, vals.pyn), pdist + 1e-4);
+            
+            prev = cur;
+            cur = 1 - cur;
+        }
+        /*CONSTRAINT(0.1, sqrt(sqdist(px[i], 15)));
+        CONSTRAINT(0.1, sqrt(sqdist(py[i], 16)));*/
     }
 
-    CONSTRAINT(vx[N-1], 1e-4);
-    CONSTRAINT(vy[N-1], 1e-4);
-    dist = x[L] - vals.ls;
-    CONSTRAINT(dist*dist, DT*DT);
-    dist = x[R] - vals.rs;
-    CONSTRAINT(dist*dist, DT*DT);
+    for (i = 1; i < N; i++) {
+        CONSTRAINT(dist(x[L+i], x[L+i-1]), DT);
+        CONSTRAINT(dist(x[R+i], x[R+i-1]), DT);
+    }
+
+    angle = atan2(dy[prev], dx[prev]);
+
+    CONSTRAINT(vx[prev], 1e-4);
+    CONSTRAINT(vy[prev], 1e-4);
+    CONSTRAINT(angle*angle, 1e-4);
+    
+    CONSTRAINT(dist(x[L], vals.ls), DT);
+    CONSTRAINT(dist(x[R], vals.rs), DT);
 
 #undef CONSTRAINT
     y += cons;
@@ -139,11 +165,11 @@ static int progress(void *instance, const lbfgsfloatval_t *x, const lbfgsfloatva
 
     printf("Iteration %d:\n", k);
     printf("  fx = %f;\n ", fx);
-    for (i = 0; i < N; i ++){
+    /*for (i = 0; i < N; i ++){
         printf("l[%d] = %.10f; r[%d] = %.10f\n", i, x[L+i], i, x[R+i]);
     }
     printf("\n");
-    printf("  xnorm = %f, gnorm = %f, step = %f\n", xnorm, gnorm, step);
+    printf("  xnorm = %f, gnorm = %f, step = %f\n", xnorm, gnorm, step);*/
     printf("\n");
     return 0;
 }
@@ -182,6 +208,8 @@ int main(int argc, char *argv[])
     /* Initialize the parameters for the L-BFGS optimization. */
     lbfgs_parameter_init(&param);
     param.linesearch = LBFGS_LINESEARCH_BACKTRACKING_WOLFE;
+    param.past = 1;
+    param.delta = 1e-8;
     //param.max_linesearch = 200;
     //param.gtol = 1e-3;
 
@@ -189,10 +217,17 @@ int main(int argc, char *argv[])
 
     printf("L-BFGS optimization terminated with status code = %d\n", ret);
     printf("  fx = %f;\n", fx);
-    for (i = 0; i < N; i ++){
+    for (i = 0; i < N; i++){
         printf("    l[%d] = %.10f; r[%d] = %.10f\n", i, x[L+i], i, x[R+i]);
     }
     printf("\n");
+
+    printf("MATLAB vectors:\n");
+    printf("l = [ ");
+    for (i = 0; i < N; i++) { printf("%f ", x[L+i]); }
+    printf(" ]\nr = [ ");
+    for (i = 0; i < N; i++) { printf("%f ", x[R+i]); }
+    printf(" ]\n");
 
     /* Answer according to Wolfram Alpha:
      *  1.00607 x^2 - 0.363643 x + 0.554
---
+++ experiments/thrust/thrustregion.m
@@ -0,0 +1,104 @@
+function thrustregion(l, r, dt)
+
+M = 100;        % Mass in kg
+R = 4;          % Radius in m
+
+p = [0 0];    % Position
+v = [0 0];      % Velocity
+d = [1 0];      % Direction
+omega = 0;      % Angular velocity
+
+iter = 20;      % ODE solver iterations
+h = dt/iter;    % Iteration step size
+
+% Inputs:
+%   T : time steps
+%   p_dest : destination
+%   p_via : point to go near
+% Independent Variables:
+%   l_k, r_k, dt_k                                    for k = 1 to T
+% Dependent Variables:
+%   px_k, py_k, vx_k, vy_k dx_k, dy_k, w_k, h_k, t_k  for k = 1 to T
+% Constraints:
+%   t_0   = 0
+%   t_k   = t_{k-1} + dt_{k-1}
+%   w_k   = w(t_k)
+%   h_k   = h(t_k)
+%   dx_k  = dx(t_k)
+%   dy_k  = dy(t_k)
+%   vx_k  = vx(t_k)
+%   vy_k  = vy(t_k)
+%   px_k  = px(t_k)
+%   py_k  = py(t_k)
+% Objective:
+%   minimize ||p_k - p_dest|| + sum{ dt_i + ||p_i-p_via|| }
+
+fig = figure( ...
+    'Name', 'Thrusters', ...
+    'NumberTitle', 'off');
+colordef(fig, 'black');
+ax = axes( ...
+    'Units', 'normalized', ...
+    'Drawmode', 'fast');
+axis([-100 100 -100 100]);
+
+count = length(l);
+for i=2:count+1
+    for j=1:iter
+        % Solve the ODE to get the next state
+        y = [p(1) p(2) v(1) v(2) d(1) d(2) omega];
+        
+        %result = ode45(@(t,y)(thrustODE(t,y,M,R,l(i),r(i))), [0 h], y0);
+        soln = step(h,y,M,R,l(i-1),r(i-1));
+        %soln = result.y(:,end);
+        p = [soln(1) soln(2)];
+        v = [soln(3) soln(4)];
+        d = [soln(5) soln(6)];
+        omega = soln(7);
+
+        % Plot the graphic
+        cla;
+        rectangle( ...,
+            'Position', [p(1)-R, p(2)-R 2*R 2*R], ...
+            'Curvature', [1 1], ...
+            'EdgeColor', 'white');
+        line([p(1) p(1)+d(1)*R], [p(2) p(2)+d(2)*R], ...
+            'Color', 'white');
+        
+        status = [sprintf('p:(%f %f)\n', p(1), p(2)) sprintf('v:(%f %f)\n', v(1), v(2)) sprintf('lr:(%f %f)\n', l(i-1), r(i-1))];
+        text(-95, 85, status);
+        drawnow;
+    end
+end
+
+% System of first order ODEs to simulate the thrust
+function dy = thrustODE(t,y,M,R,l,r)
+dy = zeros(5, 1);
+dy(1) = y(3);           % p_x' = v_x
+dy(2) = y(4);           % p_y' = v_y
+dy(3) = (l+r)*y(5)/M;   % v_x' = a_x = (l+r)d_x/M;
+dy(4) = (l+r)*y(6)/M;   % v_x' = a_y = (l+r)d_y/M;
+dy(5) = -y(7) * y(6);   % d_x' = -omega * d_y
+dy(6) = y(7) * y(5);    % d_y' = omega * d_x
+                        % omega' = 2*R*(l-r) / (M*R^2)
+dy(7) = (2 * (l - r)) / (M * R);
+
+function y = step(dt,y,M,R,l,r)
+lasty = y;
+dw = 2 * (l - r) / (M * R);
+y(7) = lasty(7) + dw * dt;
+t = 0.5 * dt * (y(7) + lasty(7));
+y(5) = lasty(5) * cos(t) - lasty(6) * sin(t);
+y(6) = lasty(5) * sin(t) + lasty(6) * cos(t);
+dnorm = norm([y(5) y(6)]);
+y(5) = y(5) / dnorm;
+y(6) = y(6) / dnorm;
+ax = lasty(5) * (l + r)/M;
+ay = lasty(6) * (l + r)/M;
+y(3) = lasty(3) + ax * dt;
+y(4) = lasty(4) + ay * dt;
+y(1) = lasty(1) + lasty(3) * dt + 0.5 * ax * dt * dt;
+y(2) = lasty(2) + lasty(4) * dt + 0.5 * ax * dt * dt;
+
+
+
